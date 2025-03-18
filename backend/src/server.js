fe0fb4ec34cde2a2a73636c5e9eec5c503f828e2
Franco Moretto <francorc06@gmail.com>
2025-03-18 17:17:13 -0300

    eliminar opciones obsoletas de conexión a MongoDB en server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,8 +10,6 @@ app.use(express.json());
 
 // Conexión a la base de datos
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
 })
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.log(err));
