370048a04ed0e1c1dd5c9e4e30657deda6d42f5b
Franco Moretto <francorc06@gmail.com>
2025-03-31 17:02:32 -0300

    actualizar configuración de conexión a MongoDB; reestructurar el servidor y agregar script de desarrollo

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,23 @@
-const express = require('express');
+const express = require("express");
+const connectDB = require("./config/db"); // Corregir la ruta del archivo
+
+const app = express();
+
+// Conectar a MongoDB
+connectDB();
+
+app.use(express.json()); // Habilitar JSON en el backend
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
+
+
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config();
-
-const app = express();
 
 app.use(express.json());
 
@@ -21,6 +34,4 @@ app.use('/api/products', require('./routes/productRoutes')); // Agregar esta lí
 app.use('/api/sales', require('./routes/saleRoutes')); // Agregar esta línea
 // Agrega más rutas aquí
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
